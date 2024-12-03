6f718626f4e973365ac670987b9d928ecddade7e
HHS <hhs3528@naver.com>
2024-12-03 14:17:37 +0900

    useStates

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+
+import Calc1 from './comp/calc/study01';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <About />
         <Routes>
           <Route path={"/"} element={<Home />}/>
           <Route path={"/about"} element={<About />}/>
+          <Route path={"/cal1"} element={<Calc1 />}/>
         </Routes>
       </BrowserRouter>
     </div>
@@ -20,15 +23,15 @@ function Home(){
   return(
     <div>
       <h1>Start Home</h1>
-      <Link to="/about">About으로 이동</Link>
+      <Link to="/about">About으로 이동</Link> <br/>
+      <Link to="/cal1">cal1으로 이동</Link>
     </div>
   )
 }
 
 function About(){
   return(
-    <div>
-      <h1>About</h1>
+    <div style={{border: '2px blue solid'}}>
       <Link to="/">Home으로 이동</Link>
     </div>
   )
